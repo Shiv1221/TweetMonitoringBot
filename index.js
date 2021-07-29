1ea96a5f12e4274ce77c6891858b7dc550ca33c0
Shiv1221 <shiv.anwane@gmail.com>
2021-07-29 16:35:03 +0530

    commit the changes

--- index.js
+++ index.js
@@ -22,29 +22,39 @@ var T = new Twit({
   var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'shivkumar.anwane95@gmail.com',
-      pass: 'Shiv.2212455'
+      user: 'shivkumar.a@legasis.in',
+      pass: 'Lega#2020'
     }
   });
   
   var mailOptions = {
-    from: 'shivkumar.anwane95@gmail.com',
-    to: 'shiv.anwane@gmail.com'
+    from: 'shivkumar.a@legasis.in',
+    to: 'shivkumar.a@legasis.in,raja.s@legasis.in'
   };
-  
-   var followId = ['987236469134712832','2613904818','2596143056','2595957175','1221365126','2665238888','2645220206','2911771819','735362544160886784','944168331279339523','2656599733','4808052715','3619207820','2602959463','2589318188','1066064184','2543217782','2650295678','2583806562','1056904937932709888','2611134289','1080769008452829184','2615251201','2907739345'];
+ 
+
+   var followId = ['987236469134712832','471741741','2613904818','2596143056','2595957175','1221365126','2665238888','2645220206','2911771819','735362544160886784','944168331279339523','2656599733','4808052715','3619207820','2602959463','2589318188','1066064184','2543217782','2650295678','2583806562','1056904937932709888','2611134289','1080769008452829184','2615251201','2907739345'];
    
    var stream = T.stream('statuses/filter', { follow: followId })
    stream.on('tweet', function (tweet) 
    {
+
      if(followId.indexOf(tweet.user.id_str) !== -1)
      {
-       if(!tweet.text.startsWith("RT @"))
+      //  console.log("pre olddd ************");
+      //  console.log(tweet);
+       if((!tweet.text.startsWith("RT @")) && (tweet.in_reply_to_status_id==null))        //
        {
-         mailOptions.text = tweet.text;
+         if(tweet.hasOwnProperty('extended_tweet'))
+         {
+          mailOptions.text = tweet.extended_tweet.full_text;
+         }else{
+          mailOptions.text = tweet.text;
+         }
+         
          mailOptions.subject = 'Tweet from @'+tweet.user.screen_name;
 
-         console.log(tweet.text)
+         console.log(tweet.text);
          console.log(tweet.user.screen_name);
     
          transporter.sendMail(mailOptions, function(error, info)
